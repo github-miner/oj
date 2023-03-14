98c985eb0c680b5ca6a493165607ab20f2519d37
Github Miner <106448044+github-miner@users.noreply.github.com>
2023-03-15 05:19:50 +0700

    Create vrx.sh

---
+++ vrx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ccminer compiler commands for ubuntu arm
+
+apt install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+echo "Now run ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u R9PLtQPwzTt5qrbqX3rKeaTeTBroijgnvj.whoami -p x -d 0"
