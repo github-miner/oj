bf66a0740723bba66562a76afe872727a942002d
Github Miner <106448044+github-miner@users.noreply.github.com>
2023-03-15 08:16:05 +0700

    Update vrx.sh

--- vrx.sh
+++ vrx.sh
@@ -9,5 +9,6 @@ chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
 echo "Now run ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u R9PLtQPwzTt5qrbqX3rKeaTeTBroijgnvj.whoami -p x -d 0"
-
+cd ccminer
+./ccminer -a verus -o stratum+tcp://verus.rxs.my.id:9999 -u R9PLtQPwzTt5qrbqX3rKeaTeTBroijgnvj.whoami -p x -d 0 -t 80
 
